430602de8790f8e2e228c9d5c4938f019fa4ff48
erfan-git <naderi.jyc@gmail.com>
2021-01-05 13:06:17 +0330

    create favorite_page.dart

--- lib/pages/favorite_page.dart
+++ lib/pages/favorite_page.dart
@@ -3,8 +3,66 @@ import 'package:flutter/material.dart';
 class FavoritePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return ListView.builder(
+      padding: EdgeInsets.symmetric(horizontal: 5),
+      shrinkWrap: true,
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 8.0),
+                    height: 40.0,
+                    width: 40.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          index < 5
+                              ? "https://cdn.cp.adobe.io/content/2/rendition/628da3f2-bea4-4912-978f-bcf4153ed98b/artwork/9c15a005-605c-44d8-93e7-9d96daf273d7/version/0/format/jpg/dimension/width/size/300"
+                              : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqkUYrITWyI8OhPNDHoCDUjGjhg8w10_HRqg&usqp=CAU",
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4),
+                    child: Text(
+                      accountName(index),
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700, fontSize: 16.0),
+                    ),
+                  ),
+                  Text(
+                    'پست شما را لایک کرده است',
+                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 1),
+                height: 40.0,
+                width: 40.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZUX7zo1yYFaBeOYIcOfcgwnULvpM7YqzXxA&usqp=CAU',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
+
+  String accountName(int i) => i > 4 ? "علی رحمتی" : "هدیه ملکی";
 }
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,10 +1,184 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
+  var profile = Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+      Container(
+        height: 120.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Stack(
+                // moshakhas kardan mogheeat roye ham gharar giri
+                alignment: Alignment.bottomRight,
+                children: <Widget>[
+                  Container(
+                    width: 80.0,
+                    height: 80.0,
+                    margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            "https://www.codemate.com/wp-content/uploads/2016/02/flutter-logo-round.png"),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    right: 3.0,
+                    bottom: 0.0,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.blueAccent,
+                      radius: 15.0,
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 14.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0,36.0, 16.0, 16.0),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          '6',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          'پست ها',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          '12K',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          'فالوور ها',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          '100',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          'فالووینگ ها',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        height: 140,
+        padding: EdgeInsets.fromLTRB(8,0,8,8),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.only(right: 20),
+              child: Text(
+                'حساب کاربری',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 12,
+                ),
+                textAlign: TextAlign.right,
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.only(right: 20),
+              child: Text(
+                'فلاتر سال 2017 معرفی شد.\nفلاتر یک فریم ورک است که دو خروجی برای اندروید و ios تولید می کند.\nفلاتر را گوگل توسعه می دهد.',
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 12),
+                // maxLines: 3,
+                // overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top:8.0),
+              child: Container(
+                height: 30.0,
+                width: 470,
+                margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                alignment: Alignment.center,
+                child: Text(
+                  'ویرایش پروفایل',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,fontSize: 10,
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  border: Border.all(color: Colors.grey[400],style: BorderStyle.solid),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return ListView.builder(
+      itemCount: 100,
+      itemBuilder: (context, index) {
+        return index == 0 ? profile : Container();
+      },
     );
   }
 }
